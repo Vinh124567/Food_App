24b9aef1aa713c9e1d4dadb7baa26bb1b48c5406
Vinh124567 <phungducvinh1245@gmail.com>
2024-08-19 15:51:46 +0700

    fix email login bug

--- app/src/main/java/com/example/myapplication/View/Fragment/LoginFragment.kt
+++ app/src/main/java/com/example/myapplication/View/Fragment/LoginFragment.kt
@@ -1,5 +1,4 @@
 package com.example.myapplication.View.Fragment
-
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
--- app/src/main/java/com/example/myapplication/View/Fragment/SingUpSuccessFragment.kt
+++ app/src/main/java/com/example/myapplication/View/Fragment/SingUpSuccessFragment.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.View.Fragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentSingUpSuccessBinding
 
@@ -14,5 +15,8 @@ class SingUpSuccessFragment : Fragment(R.layout.fragment_sing_up_success) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentSingUpSuccessBinding.bind(view)
+        binding.btnFindFood.setOnClickListener {
+            findNavController().navigate(R.id.action_singUpSuccessFragment_to_homeFragment2)
+        }
     }
 }
